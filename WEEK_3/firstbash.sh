19c61298761fb1b08f1431a59279c59cbe7b8342
jeshurunbiney <116451716+jeshurunbiney@users.noreply.github.com>
2023-11-21 17:31:39 -0800

    Add files via upload
    added week 3 assignments

---
+++ WEEK_3/firstbash.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash 
+echo "Welcome the the Biggest Little City"
+echo "This city is known as RENO"
+echo "Reno is located in Nevada"
+
---
+++ WEEK_3/rsync_bash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rsync -av /Users/jeshurunbiney/BIOL792 /Users/jeshurunbiney/Desktop
+
